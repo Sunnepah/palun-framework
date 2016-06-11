4554a5b74af9d00596848ba4b509701893b6106f
Sunday Ayandokun <sunday.ayandokun@gmail.com>
2016-06-12 01:07:33 +0300

    Added test to ensure files structure exist as specified in design note.md

---
+++ tests/FileStructureTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: sunnepah
+ * Date: 09/06/16
+ * Time: 4:42 PM
+ */
+
+class fileStructureTest extends TestCase
+{
+    
+    public function testApplicationDirectoryExist()
+    {
+        $this->assertTrue(file_exists(__DIR__ . '/../application'));
+    }
+
+    public function testControllersDirectoryExist()
+    {
+        $this->assertTrue(file_exists(__DIR__ . '/../application/Controllers/'));
+    }
+
+    public function testModelsDirectoryExist()
+    {
+        $this->assertTrue(file_exists(__DIR__ . '/../application/Models/'));
+    }
+
+    public function testConfigDirectoryExist()
+    {
+        $this->assertTrue(file_exists(__DIR__ . '/../application/Config/'));
+    }
+
+    public function testSrcDirectoryExist()
+    {
+        $this->assertTrue(file_exists(__DIR__ . '/../application/src/'));
+    }
+
+    public function testLibrariesDirectoryExist()
+    {
+        $this->assertTrue(file_exists(__DIR__ . '/../application/Libraries/'));
+    }
+    
+    public function testPublicRootDirectoryExist()
+    {
+        $this->assertTrue(file_exists(__DIR__ . '/../web/'));
+    }
+}
