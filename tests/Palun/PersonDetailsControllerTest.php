0ffdb974dd10d46d6e19aadc13c8d0dcb39cbe6d
Sunday Ayandokun <sunday.ayandokun@gmail.com>
2016-06-13 22:48:24 +0300

    Added test to list all person details

---
+++ tests/Palun/PersonDetailsControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: sunnepah
+ * Date: 6/13/16
+ * Time: 10:23 PM
+ */
+class PersonDetailsControllerTest extends TestCase
+{
+
+    protected $client;
+
+    protected function setUp()
+    {
+        $this->client = new GuzzleHttp\Client([
+            'base_uri' => 'http://0.0.0.0:8080'
+        ]);
+    }
+
+    public function test_get_all_address_list_endpoint_returns_200() {
+        $response = $this->client->get('/address');
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $data = json_decode($response->getBody(), true);
+
+        $this->assertNotNull($data);
+    }
+}
