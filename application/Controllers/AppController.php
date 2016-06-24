32e8a0b9203bd26bad23491393231242264599a0
Sunday Ayandokun <sunday.ayandokun@gmail.com>
2016-06-24 22:13:14 +0300

    Added Default Application controller

---
+++ application/Controllers/AppController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sunnepah
+ * Date: 6/24/16
+ * Time: 7:22 PM
+ */
+
+namespace Application\Controllers;
+
+
+class AppController
+{
+    public function index() {
+        return json_encode(['App' => 'Welcome!']);
+    }
+}
