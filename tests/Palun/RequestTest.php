109fb13bb71b368bd03a091fe3e51adb5995338d
Sunday Ayandokun <sunday.ayandokun@gmail.com>
2016-07-02 22:57:23 +0300

    Added request class to represent basic  Http requests properties

--- tests/Palun/RequestTest.php
+++ tests/Palun/RequestTest.php
@@ -14,17 +14,39 @@ class RequestTest extends \TestCase
     
     function setUp () {
         /** Set HTTP request properties */
-        $_SERVER['REQUEST_URI'] = '/';
         $_SERVER['REQUEST_METHOD'] = 'GET';
         $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
-
-        $this->request = new Request();
     }
     
     public function test_request_has_properties() {
+        $_SERVER['REQUEST_URI'] = '/';
+        
+        $this->request = new Request();
         
         $this->assertEquals("/", $this->request->url);
         $this->assertEquals("GET", $this->request->method);
-        $this->assertEquals("", $this->request->referrer);
+    }
+
+    public function test_get_request_pass_query_properties() {
+        $_SERVER['REQUEST_URI'] = '/address?id=1';
+
+        $this->request = new Request();
+        
+        $this->assertEquals("/address?id=1", $this->request->url);
+        $this->assertEquals("GET", $this->request->method);
+    }
+
+    public function test_post_request_pass_query_properties() {
+        $_SERVER['REQUEST_URI'] = '/address';
+        $_SERVER['REQUEST_METHOD'] = 'POST';
+        
+        $_POST = ["names" => "Michal","number" => "506088156",  "street" => "Michalowskiego 41"];
+
+        $this->request = new Request();
+
+        $this->assertEquals("/address", $this->request->url);
+        $this->assertEquals("POST", $this->request->method);
+        
+        $this->assertNotEmpty($this->request->data);
     }
 }
