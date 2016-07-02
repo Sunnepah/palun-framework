645c413e4ea7b183ec95dd56044a42b15e52cd22
Sunday Ayandokun <sunday.ayandokun@gmail.com>
2016-07-02 23:20:24 +0300

    Added request to Controller class to access request properties

--- application/Controllers/AddressesController.php
+++ application/Controllers/AddressesController.php
@@ -12,10 +12,12 @@ namespace Application\Controllers;
 use Application\Libraries\AddressManager;
 use Application\Repositories\AddressRepository;
 use Palun\Database\Database;
+use Palun\Request;
 
 class AddressesController
 {
     protected $address;
+    private $request;
     
     public function __construct()
     {   
@@ -25,25 +27,49 @@ class AddressesController
         $db = new Database();
         $addressRepo = new AddressRepository($db);
         $this->address = new AddressManager($addressRepo);
+        $this->request = new Request();
     }
 
-    public function get($id) {
+    public function get() {
 
-        $response = $this->address->getAddress($id);
+        if (!isset($this->request->query['id']) || empty($this->request->query['id'])) {
+            http_response_code(400);
+            return \GuzzleHttp\json_encode([]);
+        }
+        
+        $response = $this->address->getAddress($this->request->query['id']);
+        
+        if ($response == null) {
+            http_response_code(404);
+            return \GuzzleHttp\json_encode([]);
+        }
 
         return json_encode($response);
     }
 
     public function getAll() {
 
-        $allAddresses = $this->address->getAllAddresses();
+        $response = $this->address->getAllAddresses();
 
-        return json_encode($allAddresses);
+        if ($response == null) {
+            http_response_code(404);
+            return \GuzzleHttp\json_encode([]);
+        }
+        
+        return json_encode($response);
     }
 
-    public function post($data) {
-
-        $response = $this->address->createAddress($data);
+    public function post() {
+        
+        if (empty($this->request->data)) {
+            http_response_code(400);
+            return \GuzzleHttp\json_encode(["message" => "Post data is empty!"]);
+        }
+        /** The data must be well validated before it gets here */
+        $response = $this->address->createAddress($this->request->data);
+        if($response == true) {
+            http_response_code(201);
+        }
 
         return json_encode($response);
     }
@@ -55,9 +81,14 @@ class AddressesController
         return json_encode($response);
     }
 
-    public function delete($id) {
+    public function delete() {
 
-        $response = $this->address->removeAddress($id);
+        if (!isset($this->request->query['id']) || empty($this->request->query['id'])) {
+            http_response_code(400);
+            return \GuzzleHttp\json_encode([]);
+        }
+        
+        $response = $this->address->removeAddress($this->request->query['id']);
 
         return json_encode($response);
     }
