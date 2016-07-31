98e0f4ec479f24ae17cc6e8abcf389b2094ca622
Sunday Ayandokun <sunday.ayandokun@gmail.com>
2016-07-31 11:42:21 +0300

    Refactored to use framework response component to handle responses in controller

--- application/Controllers/AddressesController.php
+++ application/Controllers/AddressesController.php
@@ -13,14 +13,17 @@ use Application\Libraries\AddressManager;
 use Application\Repositories\AddressRepository;
 use Lustre\Database\Database;
 use Lustre\Request;
+use Lustre\Response;
 
 class AddressesController
 {
     protected $address;
     private $request;
-    
-    public function __construct()
-    {   
+
+    /**
+     * AddressesController constructor.
+     */
+    public function __construct() {   
         /*
          * This will be properly implemented using Dependency injection
          */
@@ -29,78 +32,98 @@ class AddressesController
         $this->address = new AddressManager($addressRepo);
         $this->request = new Request();
     }
-
+    
+    /**
+     * Retrieve an address
+     * 
+     * @return \Lustre\Response
+     */
     public function get() {
 
         if (!isset($this->request->query['id']) || empty($this->request->query['id'])) {
-            http_response_code(400);
-            return \GuzzleHttp\json_encode([]);
+            return (new Response(null, 400))->json();
         }
         
         $response = $this->address->getAddress($this->request->query['id']);
         
         if ($response == null) {
-            http_response_code(404);
-            return \GuzzleHttp\json_encode([]);
+            return (new Response(null, 404))->json();
         }
 
-        return json_encode($response);
+        return (new Response($response, 200))->json();
     }
 
+    /**
+     * Retrieve all addresses.
+     * 
+     * @return \Lustre\Response
+     */
     public function getAll() {
 
         $response = $this->address->getAllAddresses();
 
         if ($response == null) {
-            http_response_code(404);
-            return \GuzzleHttp\json_encode([]);
+            return (new Response(null, 400))->json();
         }
-        
-        return json_encode($response);
+
+        return (new Response($response, 200))->json();
     }
 
+    /**
+     * Create a new address
+     * 
+     * @return \Lustre\Response
+     */
     public function post() {
         
         if (empty($this->request->data)) {
-            http_response_code(400);
-            return \GuzzleHttp\json_encode(["message" => "Post data is empty!"]);
+            return (new Response(["message" => "Post data is empty!"], 400))->json();
         }
+        
         /** The data must be well validated before it gets here */
         $response = $this->address->createAddress($this->request->data);
         if($response == true) {
-            http_response_code(201);
+            return (new Response($response, 201))->json();
         }
 
-        return json_encode($response);
+        return (new Response($response, 500))->json();
     }
 
+    /**
+     * Update an address
+     * 
+     * @return \Lustre\Response
+     */
     public function put() {
-
-        $id = $this->request->query['id'];
-        if (!isset($id) || empty($this->request->query['id'])) {
-            http_response_code(400);
-            return \GuzzleHttp\json_encode(["Message" => "Missing resource id"]);
+        
+        if (!isset($this->request->query['id']) || empty($this->request->query['id'])) {
+            return (new Response(["Message" => "Missing resource id"], 400))->json();
         }
 
         $data = $this->request->data;
         if (empty($data)) { 
-            http_response_code(400);
-            return \GuzzleHttp\json_encode(["message" => "Data is empty!"]);
+            return (new Response(["message" => "Data is empty!"], 400))->json();
         }
-        $response = $this->address->updateAddress($id, $data);
+        
+        $response = $this->address->updateAddress($this->request->query['id'], $data);
 
-        return json_encode($response);
+        return (new Response($response, 200))->json();
     }
 
+    /**
+     * Remove an address
+     * 
+     * @return \Lustre\Response
+     */
     public function delete() {
 
         if (!isset($this->request->query['id']) || empty($this->request->query['id'])) {
-            http_response_code(400);
-            return \GuzzleHttp\json_encode([]);
+            
+            return (new Response(null, 400))->json();
         }
         
         $response = $this->address->removeAddress($this->request->query['id']);
 
-        return json_encode($response);
+        return (new Response($response, 200))->json();
     }
 }
